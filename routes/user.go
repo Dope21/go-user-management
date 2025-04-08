6ad1a69b7878838c4204ba3612dc75d9bb65f3f5
Dope21 <thanasaklimsila@gmail.com>
2025-04-08 13:21:58 +0700

    fix: user -> users

--- routes/user.go
+++ routes/user.go
@@ -8,9 +8,9 @@ import (
 )
 
 func UserRouter(r *mux.Router) {
-	r.HandleFunc("/user", handlers.GetAllUser).Methods(http.MethodGet)
-	r.HandleFunc("/user/register", handlers.RegisterUser).Methods(http.MethodPost)
-	r.HandleFunc("/user/{user_id}", handlers.UpdateUserByID).Methods(http.MethodPatch)
-	r.HandleFunc("/user/{user_id}", handlers.GetUserByID).Methods(http.MethodGet)
-	r.HandleFunc("/user/{user_id}", handlers.DeleteUserByID).Methods(http.MethodDelete)
+	r.HandleFunc("/users", handlers.GetAllUser).Methods(http.MethodGet)
+	r.HandleFunc("/users/register", handlers.RegisterUser).Methods(http.MethodPost)
+	r.HandleFunc("/users/{user_id}", handlers.UpdateUserByID).Methods(http.MethodPatch)
+	r.HandleFunc("/users/{user_id}", handlers.GetUserByID).Methods(http.MethodGet)
+	r.HandleFunc("/users/{user_id}", handlers.DeleteUserByID).Methods(http.MethodDelete)
 }
